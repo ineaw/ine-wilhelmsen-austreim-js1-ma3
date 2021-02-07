5143d541e35ccbfc5ef1122b919b13cb303208d9
ineaw <ineaw@users.noreply.github.com>
2021-02-07 22:38:42 +0100

    Finished

--- js/question-2.js
+++ js/question-2.js
@@ -1,5 +1,4 @@
-const url =
-  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const url ="https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const resultContainer = document.querySelector(".games");
 
 async function getGames() {
